6294824146df3d06474a26635b07610d7bd56e08
RevaLaxmi <Reva.chauhan1@gmail.com>
2025-02-13 19:37:14 +0530

    testing startung page vibe pt 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import "../styles/globals.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>💖 Valentine's Memory Game 💖</h1>
+      <h1>💖 Valentines Memory Game 💖</h1>
       <Link href="/game">
         <button>Start Game 🎮</button>
       </Link>
